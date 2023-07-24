a8fbced72ab329910142efd50d2bb69dd93ecbe0
Hy0kR1 <90767483+0xHy0kR1@users.noreply.github.com>
2023-07-24 08:26:51 +0530

    Updates

--- module1.mjs
+++
@@ -1,5 +0,0 @@
-import ui, {a, c, d} from './module2.mjs' // Here we can't export "a", "c", "d" by default that's why we can't use different names other then one we specified in module2.j
-console.log(ui);
-console.log(c);
-console.log(a);
-console.log(d);
--- module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Harry";
-const b = "Rohan";
-const c = "Aakash";
-const d = "Priyanka";
-
-export default d; // Exporting "Priyanka" as "d"
-export {a}; // Here, we can't export "a" by default that's why we use "{}"
-export {c}; // same
-export {d}; // same
